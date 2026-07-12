9a20e680908995f818dfae9228a3be4ef8883dbf
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "mssql_job_schedules" {
-  description = "All mssql_job_schedule resources"
-  value       = azurerm_mssql_job_schedule.mssql_job_schedules
-}
 output "mssql_job_schedules_enabled" {
-  description = "List of enabled values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.enabled]
+  description = "Map of enabled values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.enabled }
 }
 output "mssql_job_schedules_end_time" {
-  description = "List of end_time values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.end_time]
+  description = "Map of end_time values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.end_time }
 }
 output "mssql_job_schedules_interval" {
-  description = "List of interval values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.interval]
+  description = "Map of interval values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.interval }
 }
 output "mssql_job_schedules_job_id" {
-  description = "List of job_id values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.job_id]
+  description = "Map of job_id values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.job_id }
 }
 output "mssql_job_schedules_start_time" {
-  description = "List of start_time values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.start_time]
+  description = "Map of start_time values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.start_time }
 }
 output "mssql_job_schedules_type" {
-  description = "List of type values across all mssql_job_schedules"
-  value       = [for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : v.type]
+  description = "Map of type values across all mssql_job_schedules, keyed the same as var.mssql_job_schedules"
+  value       = { for k, v in azurerm_mssql_job_schedule.mssql_job_schedules : k => v.type }
 }
 
